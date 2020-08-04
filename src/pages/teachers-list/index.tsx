bfc62f3090b1082e5f12973e3918036b83a3a5bb
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-08-03 22:33:19 -0300

    Adds teachers list to teachers page

--- src/pages/teachers-list/index.tsx
+++ src/pages/teachers-list/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/page-header';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
 import './styles.css';
 
 function TeachersList() {
@@ -25,6 +27,35 @@ function TeachersList() {
                     </div>
                 </form>
             </PageHeader>
+
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="placeholder" alt="Proffy"/>
+                        <div>
+                            <strong>Vitor Fernandes</strong>
+                            <span>Computação</span>
+                        </div>
+                    </header>
+                    
+                    <p>
+                        Entusiasta por desenvolvimento de games
+                        <br /><br />
+                        Já desenvolveu alguns jogos com tecnologias Web, utilizando de um framework criado por ele mesmo.
+                    </p>
+
+                    <footer>
+                        <p>
+                            Preço/hora
+                            <strong>R$ 80,00</strong>
+                        </p>
+                        <button type="button">
+                            <img src={whatsappIcon} alt="Whatsapp" />
+                            Entrar em contato
+                        </button>
+                    </footer>
+                </article>
+            </main>
         </div>
     );
 }
